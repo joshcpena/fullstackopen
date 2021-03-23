61452ff719e517a275e8ee9cf40e6872ab13468c
jcp105 <jcp105@georgetown.edu>
2021-03-23 15:31:55 -0400

    linted person.js

--- part3/Phonebook_Backend/models/person.js
+++ part3/Phonebook_Backend/models/person.js
@@ -1,38 +1,42 @@
+/* eslint-disable no-console */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
+
 const uniqueVal = require('mongoose-unique-validator');
-const url = process.env.MONGODB_URI
+
+const url = process.env.MONGODB_URI;
 console.log('connecting to:', url);
 
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(result => console.log('connected to MongoDB'))
-    .catch(error => console.log('error connecting to MongoDB', error));
-
-
+mongoose.connect(url, {
+  useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true,
+})
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB', error));
 
 const personSchema = new mongoose.Schema({
-    id: Number,
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: 3
-    },
-    number: {
-        type: String,
-        minlength: 8
-    }
+  id: Number,
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+  },
 
 });
 
 personSchema.plugin(uniqueVal);
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
