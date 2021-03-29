6a342ad1c4f57d64686f12f30ed93234645a4d7a
jcp105 <jcp105@georgetown.edu>
2021-03-29 18:11:28 -0400

    added conditional login logic

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import LoginForm from './components/LoginForm'
+import Notification from './components/Notification'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [message, setMessage] = useState(null)
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -24,24 +26,32 @@ const App = () => {
       setUsername('');
       setPassword('')
     } catch (exception) {
-      console.log('error loggin in');
+      setMessage('Wrong credentials')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     }
   }
 
   return (
     <div>
+      <Notification message={message} />
       {user === null ?
         <LoginForm
           handleLogin={handleLogin}
           username={username}
           setUsername={setUsername}
           password={password}
-          setPassword={setPassword} />
-        : 'please sign in'}
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+          setPassword={setPassword}
+        /> :
+        <div>
+          <h2>blogs</h2>
+          <p>{user.username} logged in </p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>}
+
     </div>
   )
 }
--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,27 +1,30 @@
 import React from 'react';
 
-const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => (
-  <form onSubmit={handleLogin}>
-    <div>
-      username
+const LoginForm = ({ handleLogin, username, setUsername, password, setPassword, errorMessage, setErrorMessage }) => (
+  <div>
+    <h2>log in to application </h2>
+    <form onSubmit={handleLogin}>
+      <div>
+        username
       <input
-        type="text"
-        value={username}
-        name="Username"
-        onChange={({ target }) => setUsername(target.value)}
-      />
-    </div>
-    <div>
-      password
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
       <input
-        type="password"
-        value={password}
-        name="Password"
-        onChange={({ target }) => setPassword(target.value)}
-      />
-    </div>
-    <button type="submit">login</button>
-  </form>
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
 )
 
 export default LoginForm;
