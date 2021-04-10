4b3ff5cc58428bc5f006397503c518203cdf3dd9
jcp105 <jcp105@georgetown.edu>
2021-04-10 19:52:37 -0400

    axios api

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,7 +18,7 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => { })
 
   return country
 }
@@ -40,8 +40,8 @@ const Country = ({ country }) => {
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`} />
     </div>
   )
 }
@@ -55,6 +55,13 @@ const App = () => {
     e.preventDefault()
     setName(nameInput.value)
   }
+
+  useEffect(() => {
+    const findCountry = async () => {
+      const result = await axios.get('https://restcountries.eu/rest/v2/name/' + name)
+
+    }
+  }, [name])
 
   return (
     <div>
