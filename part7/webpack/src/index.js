4acce7c67b185280fe562852fe2e70596db0d063
jcp105 <jcp105@georgetown.edu>
2021-04-15 15:19:29 -0400

    webpack config

--- part7/webpack/src/index.js
+++ part7/webpack/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App';
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
