0a5951e2f044879c89a9f12a08fd2a759fc1e85c
jcp105 <jcp105@georgetown.edu>
2021-04-21 20:28:34 -0400

    added Links to individual user view

--- part7/blog-list/bloglist-frontend/src/components/Users.js
+++ part7/blog-list/bloglist-frontend/src/components/Users.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 const Users = () => {
   const { id } = useParams();
@@ -10,6 +10,9 @@ const Users = () => {
     (acc.get(e.user.username) || 0) + 1), new Map()));
   userList = [...userList.entries()];
 
+  const userIds = new Map(blogs.reduce((acc, e) => acc.set(e.user.username,
+    e.user.id), new Map()));
+
   // individual user view if (id) and found blog where that user id exists
   if (id && blogs.filter((item) => item.user.id === id).length > 0) {
     const userBlogs = blogs.filter((item) => item.user.id === id);
@@ -17,7 +20,7 @@ const Users = () => {
       <div>
         <h2>{userBlogs[0].user.name}</h2>
         <h3>added blogs:</h3>
-        {userBlogs.map((blog) => <li key={blog.id}>{blog.title}</li>)}
+        {userBlogs.map((blog) => <li key={blog.id}>{`${blog.title} by ${blog.author}`}</li>)}
       </div>
     );
   }
@@ -34,7 +37,9 @@ const Users = () => {
           </tr>
           {userList.length ? userList.map((user) => (
             <tr key={user[0]}>
-              <td>{user[0]}</td>
+              <td>
+                <Link to={`/users/${userIds.get(user[0])}`}>{user[0]}</Link>
+              </td>
               <td>{user[1]}</td>
             </tr>
           )) : null}
